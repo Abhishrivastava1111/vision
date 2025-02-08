dda5450148b69b3aa2de58e91dc4da052758c85f
anand.singh <bhadouriyaprince11@gmail.com>
2025-02-08 22:47:02 +0530

    Added listOfFilesPagination

--- src/main/java/com/crack/vision/doc/repo/FileRepo.java
+++ src/main/java/com/crack/vision/doc/repo/FileRepo.java
@@ -1,9 +1,14 @@
 package com.crack.vision.doc.repo;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crack.vision.doc.entity.File;
 
 public interface FileRepo extends JpaRepository<File, Integer> {
 
+    Page<File> getListOfFilesByUserId(Integer userId,Pageable pageable);
 }
